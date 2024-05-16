1196f22671325f9f4a970bbfb199c9c4ab499b4e
pbwbb <151950149+pbwbb@users.noreply.github.com>
2024-05-15 22:29:09 -0300

    Update ddg_url_extract.py

--- ddg_url_extract.py
+++ ddg_url_extract.py
@@ -2,16 +2,22 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from bs4 import BeautifulSoup
 import urllib.parse
-
+print("Duckduckgo html URL extractor by Pedro Webber -> https://github.com/pbwbb/ddg_url_extract")
 file_path = askopenfilename()
 with open(file_path, 'r',encoding='utf-8') as file:
     html = file.read()
 
 soup = BeautifulSoup(html, 'html.parser').find_all("a", href=True)
 for link in soup:
-    url = link['href'].split('=')[1].strip()
-    url = url.split('&rut')[0].strip()
-    decoded_url = urllib.parse.unquote(link)
-    with open('extracted_urls.txt', 'a',encoding='utf-8') as file:
-        file.write(decoded_url + '\n')
+    try:
+        url = link['href'].split('uddg=')[1]
+        print(url)
+        url = url.split('&rut')[0].strip()
+        decoded_url = urllib.parse.unquote(url)
+        with open('extracted_urls.txt', 'a',encoding='utf-8') as file:
+            file.write(decoded_url + '\n')
+    except:
+        pass
+        
+
     
